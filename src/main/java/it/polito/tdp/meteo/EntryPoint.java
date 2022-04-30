93f22ee566b378eb38415399db853cd3edcb4ca7
Marco Camaggio <100542819+mcamaggio@users.noreply.github.com>
2022-04-30 15:43:19 +0200

    2022/04/30

--- src/main/java/it/polito/tdp/meteo/EntryPoint.java
+++ src/main/java/it/polito/tdp/meteo/EntryPoint.java
@@ -1,7 +1,9 @@
 package it.polito.tdp.meteo;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
+//import static javafx.application.Application.launch;
+
+import it.polito.tdp.meteo.model.Model;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,12 +21,15 @@ public class EntryPoint extends Application {
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
+        
         scene.getStylesheets().add("/styles/Styles.css");
         scene.getRoot().setStyle("-fx-font-family: 'serif'");
 
         controller = loader.getController();
     	
         // TODO impostare il model nel controller        
+        Model model = new Model();
+        controller.setModel(model);
         
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
--- src/main/java/it/polito/tdp/meteo/FXMLController.java
+++ src/main/java/it/polito/tdp/meteo/FXMLController.java
@@ -5,7 +5,11 @@
 package it.polito.tdp.meteo;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.meteo.model.Citta;
+import it.polito.tdp.meteo.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,6 +17,8 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	private Model model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -21,7 +27,7 @@ public class FXMLController {
     private URL location;
 
     @FXML // fx:id="boxMese"
-    private ChoiceBox<?> boxMese; // Value injected by FXMLLoader
+    private ChoiceBox<Integer> boxMese; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnUmidita"
     private Button btnUmidita; // Value injected by FXMLLoader
@@ -34,11 +40,34 @@ public class FXMLController {
 
     @FXML
     void doCalcolaSequenza(ActionEvent event) {
+    	txtResult.clear();
+    	
+    	Integer m = boxMese.getValue(); //int cannot be null, so I use Integere here to then allow the control below
+    	
+    	if(m!=null) {
+    		List<Citta> best = model.trovaSequenza(m);
+    		
+    		txtResult.appendText(String.format("Sequenza ottima per il mese %s\n", Integer.toString(m)));
+    		txtResult.appendText(best + "\n");
+    	}
 
     }
 
     @FXML
     void doCalcolaUmidita(ActionEvent event) {
+    	txtResult.clear();
+    	
+    	//Month m = boxMese.getValue();
+    	Integer m = boxMese.getValue(); //int cannot be null, so I use Integere here to then allow the control below
+    	
+    	if(m!=null) {
+    		txtResult.appendText(String.format("Dati del mese: %s\n", Integer.toString(m)));
+    		
+    		for(Citta c : model.getLeCitta()) {
+    			double u = model.getUmiditaMedia(m, c);
+    			txtResult.appendText(String.format("Città %s: umidità %f\n", c.getNome(), u));
+    		}
+    	}
 
     }
 
@@ -49,6 +78,18 @@ public class FXMLController {
         assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
+    }
+    
+    private void setChoiceMesi() {
+    	for(int mese = 1; mese <= 12; mese++) {
+    		boxMese.getItems().add(mese);
+    		//boxMese.getItems().add(Month.of(mese));
+    	}
+    }
+    
+    public void setModel (Model m) {
+    	this.model = m;
+    	setChoiceMesi();
     }
 }
 
